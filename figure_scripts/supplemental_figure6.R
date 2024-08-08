21a45ba8f901cc8777dda53f836fbd5deaaa7d67
Andrew Oliver <46385978+aoliver44@users.noreply.github.com>
2024-08-08 11:36:56 -0700

    add missing library - vegan

--- figure_scripts/supplemental_figure6.R
+++ figure_scripts/supplemental_figure6.R
@@ -14,6 +14,7 @@ library(dplyr)
 library(picante)
 library(ggplot2)
 library(wesanderson)
+library(vegan)
 
 ## set working directory =======================================================
 setwd("/home/")
